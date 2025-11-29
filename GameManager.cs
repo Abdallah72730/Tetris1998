8520c0f90082b2e868d97596c37c67aa92b261de
Abdallah72730 <ianomeit@gmail.com>
2025-11-28 20:16:17 -0700

    Chnaged code to enhance functions

--- GameManager.cs
+++ GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+using System.Windows.Controls.Primitives;
 
 namespace Tetris1998
 {
@@ -33,14 +35,94 @@ namespace Tetris1998
         public void StartGame() 
         {
             GameGrid.Reset();
+            isGameOver = false;
+            isPaused = false;
+            score = 0;
+            SpawnNewBlock();
 
 
         }
 
         public void SpawnNewBlock() 
         {
-            CurrentBlock = 
+            int blockIndex = RandomInteger.Next(0, TetrisBlock.AllBlocks.Length);
+            int[,] block = TetrisBlock.AllBlocks[blockIndex];
+
+            int StartRow = 0;
+            int StartCol = GameGrid.Columns / 2 - 1;
+
+            if (CurrentBlock == null)
+            {
+                CurrentBlock = new Tetromino(block, StartRow, StartCol, blockIndex + 1);
+            }
+            else 
+            {
+                CurrentBlock.Reset(block, StartRow, StartCol, blockIndex+1);
+            }
+
+            if (!GameGrid.CanPlaceBlock(block, StartRow, StartCol)) 
+            {             
+                isGameOver = true;
+
+            }
         }
+
+        public void MoveBlockLeft() 
+        {
+            if (CurrentBlock == null) return;
+
+            CurrentBlock.MoveLeft();
+
+            if (!GameGrid.CanPlaceBlock(CurrentBlock.getCurrentBlock(), CurrentBlock.getCurrentRow(), CurrentBlock.getCurrentColumn()-1)) 
+            {
+                CurrentBlock.MoveRight();
+                LockBlock();
+            }
+        }
+
+        public void MoveBlockRight() 
+        {
+            if (CurrentBlock == null) return;
+
+            CurrentBlock.MoveRight();
+            if (!GameGrid.CanPlaceBlock(CurrentBlock.getCurrentBlock(), CurrentBlock.getCurrentRow(), CurrentBlock.getCurrentColumn() + 1)) 
+            {
+                CurrentBlock.MoveLeft();
+                LockBlock();
+            }
+        }
+
+        public void MoveBlockDown() 
+        {
+            if (CurrentBlock == null) return;
+
+            CurrentBlock.MoveDown();
+
+            if (!GameGrid.CanPlaceBlock(CurrentBlock.getCurrentBlock(), CurrentBlock.getCurrentRow() + 1, CurrentBlock.getCurrentColumn())) 
+            {
+                CurrentBlock.MoveUp();
+                LockBlock();
+            }
+        }
+
+        public void LockBlock() 
+        {
+            GameGrid.PlaceBlock(CurrentBlock.getCurrentBlock(), CurrentBlock.getCurrentRow(), CurrentBlock.getCurrentColumn());
+        }
+
+        public void  Update(double deltaTime) 
+        {
+            if (isGameOver || isPaused) return;
+
+            timeSinceLastFall += deltaTime;
+
+            if(timeSinceLastFall >= fallSpeed) 
+            {
+                MoveBlockDown();
+                timeSinceLastFall = 0;
+            }
+        }
+
 
         
     }
